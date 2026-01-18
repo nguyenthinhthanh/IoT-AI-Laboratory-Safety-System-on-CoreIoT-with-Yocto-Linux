7aeb9f411432ad13285dd372edf354e2667bcb2c
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-01-18 15:49:02 +0700

    Add hello-c app simple for init recipies-app

---
+++ src/hello-app/hello-c/src/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void hello_print(void)
+{
+    printf("Hello Yocto!\n");
+}
---
+++ src/hello-app/hello-c/src/main.c
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+int main(void)
+{
+    hello_print();
+    return 0;
+}
