d6185360a10ae2b05f1cb337c7c6b69b29a901f0
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-01-18 18:59:00 +0700

    Add lsmy-hello C library

---
+++ src/lsmy-hello/include/lsmy_hello.h
@@ -0,0 +1,6 @@
+#ifndef LSMY_HELLO_H
+#define LSMY_HELLO_H
+
+void hello_print(void);
+
+#endif
---
+++ src/lsmy-hello/src/lsmy_hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lsmy_hello.h"
+
+void hello_print(void)
+{
+    printf("Hello world from C!\n");
+}
