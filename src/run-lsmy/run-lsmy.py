73b727b39354e2ee573384f63931e439485c9136
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2026-01-19 18:08:06 +0700

    Import python library file for test

--- src/run-lsmy/run-lsmy.py
+++ src/run-lsmy/run-lsmy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import ctypes
 
+from lsmy_python_lib.hello import say_hello
+
 # Load shared library
 lib = ctypes.CDLL("/usr/lib/liblsmy_hello.so.1")
 
@@ -10,3 +12,6 @@ lib.hello_print.argtypes = []
 
 # Call the function from the shared library
 lib.hello_print()
+
+# Call the Python function
+say_hello()
